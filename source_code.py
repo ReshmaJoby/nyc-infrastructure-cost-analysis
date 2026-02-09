d5a98272e27e86f9b548f5a7b21d9d5d5e979dbf
ReshmaJoby <reshmaakjoby@gmail.com>
2026-02-09 13:28:38 +0000

    Delete source_code.py

--- source_code.py
+++
@@ -1,7 +0,0 @@
-# Load Libraries
-import pandas as pd
-import numpy as np
-
-# Load raw dataset
-project_data = pandas.read_csv("Capital_Projects_Dashboard_-_Citywide_Budget_and_Schedule_20260209.csv")
-print(project_data.head())
