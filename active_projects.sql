87ef5f62b0cb4ab37d793a2ac36b64758f3aa994
ReshmaJoby <reshmaakjoby@gmail.com>
2026-02-11 11:00:57 +0000

    Add files via upload

---
+++ active_projects.sql
@@ -0,0 +1,12 @@
+/* SQL Script to Filter Active Projects 
+   Targets: Pre-Design, Design, Procurement, and Construction phases.
+*/
+
+SELECT 
+    "FMS ID", "FMS Project Name", "Managing Agency", "Current Phase", "Total Budget", "Spend to Date", "Spend to Date (%)", "Actual Construction Start", "Forecast Completion", "Ten Year Plan Category"
+FROM 
+    project_sql_table
+WHERE 
+    "Current Phase" IN ('Pre-Design', 'Design', 'Construction Procurement', 'Construction')
+    AND "Actual Construction End" IS NULL
+    AND "Actual Construction Start" IS NOT NULL;
